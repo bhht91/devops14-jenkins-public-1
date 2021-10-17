77a7bbd373a94242a39b89acc8b65ecf398bf393
Vakhob <63296338+Vakhob@users.noreply.github.com>
2021-10-17 09:16:30 +0300

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=3.34.0,<=3.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  profile = "jenkins"
+}
+
+resource "aws_instance" "my-first-ec2" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  tags = {
+    Name  = "myec2-1"
+    Owner = "Vakhob"
+  }
+}
+
+resource "aws_instance" "my-second-ec2" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+  tags = {
+    Name  = "myec2-2"
+    Owner = "Vakhob"
+  }
+}
