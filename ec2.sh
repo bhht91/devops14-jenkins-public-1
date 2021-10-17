8714fc9e7138f429c05a9035127e6eb1afd35fc4
Vakhob <63296338+Vakhob@users.noreply.github.com>
2021-10-17 07:43:14 +0300

    Update ec2.sh

--- ec2.sh
+++ ec2.sh
@@ -1,4 +1,2 @@
-!/bin/sh
 # What does this script do: It launches an ec2 instance in my aws account 
-
 aws ec2 run-instances --image-id ami-02e136e904f3da870 --count 1 --instance-type t2.micro --key-name DevOps14 --region us-east-1
